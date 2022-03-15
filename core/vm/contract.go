577a881a3b3535a19a3ad051769854694f08cbc2
864685021@qq.com <864685021@qq.com>
2022-03-15 15:17:09 +0800

    add verify code

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -106,7 +106,7 @@ func (c *Contract) isCode(udest uint64) bool {
 	// Do we have a contract hash already?
 	// If we do have a hash, that means it's a 'regular' contract. For regular
 	// contracts ( not temporary initcode), we store the analysis in a map
-	if c.CodeHash != (common.Hash{}) {
+	if c.CodeHash != (common.Hash{}) && c.CodeHash != emptyCodeHash {
 		// Does parent context have the analysis?
 		analysis, exist := c.jumpdests[c.CodeHash]
 		if !exist {
