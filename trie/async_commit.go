60a3cda277c6d3c2fa970302de2a232e41b4fca9
lyh169 <liuyuhang203@sina.com>
2023-03-14 14:33:53 +0800

    add ac

---
+++ trie/async_commit.go
@@ -0,0 +1,198 @@
+package trie
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb"
+	"sync"
+)
+
+const (
+	batchChanLen = 100000
+)
+
+type keyvalue struct {
+	key    []byte
+	value  []byte
+	delete bool
+}
+
+type ACProcessor struct {
+	kvdatas chan []*keyvalue
+	cache   *Cache
+	diskdb  ethdb.KeyValueStore
+}
+
+func NewACProcessor(db ethdb.KeyValueStore) *ACProcessor {
+	ac := &ACProcessor{
+		kvdatas: make(chan []*keyvalue, batchChanLen),
+		cache:   NewCache(),
+		diskdb:  db,
+	}
+	go ac.ACCommit()
+	return ac
+}
+
+func (ac *ACProcessor) Close() {
+	close(ac.kvdatas)
+}
+
+func (ac *ACProcessor) ACCommit() {
+	for kvs := range ac.kvdatas {
+		batch := ac.diskdb.NewBatch()
+		for _, kv := range kvs {
+			if kv.delete {
+				batch.Delete(kv.key)
+			} else {
+				batch.Put(kv.key, kv.value)
+			}
+		}
+		//valueSize := batch.ValueSize()
+		//tm1 := time.Now()
+		batch.Write()
+		//tm2 := time.Now()
+		ac.cache.Clear(kvs)
+		//tm3 := time.Now()
+		//fmt.Println("ACCommit ac chan remain", len(ac.kvdatas), "batch value size", valueSize,
+		//	"write cost time", tm2.Sub(tm1),
+		//	"clear cost time", tm3.Sub(tm2),
+		//)
+	}
+}
+
+func (ac *ACProcessor) GetDirty(key common.Hash) (*cachedNode, bool) {
+	return ac.cache.GetDirty(key)
+}
+
+func (ac *ACProcessor) GetPreimage(key common.Hash) ([]byte, bool) {
+	return ac.cache.GetPreimage(key)
+}
+
+func (ac *ACProcessor) SetDirty(key common.Hash, node *cachedNode) {
+	ac.cache.SetDirty(key, node)
+}
+
+func (ac *ACProcessor) SetPreimages(images map[common.Hash][]byte) {
+	ac.cache.SetPreimages(images)
+}
+
+type Cache struct {
+	dirties   map[common.Hash]*cachedNode
+	preimages map[common.Hash][]byte
+	lock      sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		dirties:   make(map[common.Hash]*cachedNode),
+		preimages: make(map[common.Hash][]byte),
+	}
+}
+
+func (c *Cache) SetDirty(key common.Hash, node *cachedNode) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.dirties[key] = node
+}
+
+func (c *Cache) GetDirty(key common.Hash) (*cachedNode, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	if n, ok := c.dirties[key]; ok {
+		return n, true
+	}
+	return nil, false
+}
+
+func (c *Cache) SetPreimages(images map[common.Hash][]byte) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	for k, v := range images {
+		c.preimages[k] = v
+	}
+}
+
+func (c *Cache) GetPreimage(key common.Hash) ([]byte, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	if v, ok := c.preimages[key]; ok {
+		return v, true
+	}
+	return nil, false
+}
+
+func (c *Cache) Clear(kvs []*keyvalue) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	for _, kv := range kvs {
+		hash := common.BytesToHash(kv.key)
+		if _, ok := c.dirties[hash]; ok {
+			delete(c.dirties, hash)
+		} else { // Need attention the preimages key preimageKey(hash)
+			delete(c.preimages, hash)
+		}
+	}
+}
+
+type BatchEx struct {
+	writes []*keyvalue
+	size   int
+	data   chan []*keyvalue
+}
+
+func NewBatchEx(kvchan chan []*keyvalue) *BatchEx {
+	return &BatchEx{
+		data: kvchan,
+	}
+}
+
+// Put inserts the given value into the key-value data store.
+func (b *BatchEx) Put(key []byte, value []byte) error {
+	b.writes = append(b.writes, &keyvalue{common.CopyBytes(key), common.CopyBytes(value), false})
+	b.size += len(key) + len(value)
+	return nil
+}
+
+// Delete removes the key from the key-value data store.
+func (b *BatchEx) Delete(key []byte) error {
+	b.writes = append(b.writes, &keyvalue{common.CopyBytes(key), nil, true})
+	b.size += len(key)
+	return nil
+}
+
+func (b *BatchEx) ValueSize() int {
+	return b.size
+}
+
+func (b *BatchEx) Reset() {
+	b.writes = b.writes[:0]
+	b.size = 0
+}
+
+func (b *BatchEx) Write() error {
+	if len(b.writes) == 0 {
+		return nil
+	}
+	b.data <- b.writes
+	return nil
+}
+
+func (b *BatchEx) Replay(w ethdb.KeyValueWriter) error {
+	for _, keyvalue := range b.writes {
+		if keyvalue.delete {
+			if err := w.Delete(keyvalue.key); err != nil {
+				return err
+			}
+			continue
+		}
+		if err := w.Put(keyvalue.key, keyvalue.value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (db *Database) Close() {
+	if db.acProcessor != nil {
+		db.acProcessor.Close()
+	}
+}
---
+++ trie/async_commit_test.go
@@ -0,0 +1,208 @@
+package trie
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func genTestImages(n int) map[common.Hash][]byte {
+	mp := make(map[common.Hash][]byte)
+	for i := 0; i < n; i++ {
+		mp[common.BytesToHash(randBytes(32))] = randBytes(32)
+	}
+	return mp
+}
+
+type testNode struct {
+	key  []byte
+	node *cachedNode
+}
+
+func genTestDirties(n int) []*testNode {
+	var tnodes []*testNode
+	for i := 0; i < n; i++ {
+		tnodes = append(tnodes, &testNode{key: randBytes(32), node: &cachedNode{node: hashNode(randBytes(32))}})
+	}
+	return tnodes
+}
+
+func genKVs(n int) []*keyvalue {
+	var kvs []*keyvalue
+	for i := 0; i < n; i++ {
+		var isdel bool
+		value := randBytes(32)
+		if rand.Int()%2 == 0 {
+			isdel = true
+			value = nil
+		}
+		kvs = append(kvs, &keyvalue{key: randBytes(32), value: value, delete: isdel})
+	}
+	return kvs
+}
+
+func TestCache(t *testing.T) {
+	cache := NewCache()
+	testImageCase := genTestImages(6)
+	cache.SetPreimages(testImageCase)
+
+	testDirtiesCase := genTestDirties(5)
+	for _, ts := range testDirtiesCase {
+		cache.SetDirty(common.BytesToHash(ts.key), ts.node)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for _, ts := range testDirtiesCase {
+			ca, re := cache.GetDirty(common.BytesToHash(ts.key))
+			assert.True(t, re)
+			assert.Equal(t, ca, ts.node)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for k, v := range testImageCase {
+			ca, re := cache.GetPreimage(k)
+			assert.True(t, re)
+			assert.Equal(t, ca, v)
+		}
+	}()
+	wg.Wait()
+
+	var kvs []*keyvalue
+	for k := range testImageCase {
+		key := make([]byte, len(k))
+		copy(key, k[:])
+		kvs = append(kvs, &keyvalue{key: key})
+	}
+	cache.Clears(kvs)
+	assert.Equal(t, len(cache.preimages), 0)
+	assert.NotEqual(t, len(cache.dirties), 0)
+
+	for _, k := range testDirtiesCase {
+		kvs = append(kvs, &keyvalue{key: k.key})
+	}
+	cache.Clears(kvs)
+	assert.Equal(t, len(cache.preimages), 0)
+	assert.Equal(t, len(cache.dirties), 0)
+}
+
+func TestACProcessor(t *testing.T) {
+	diskdb := memorydb.New()
+	acp := NewACProcessor(diskdb)
+
+	batch := NewBatchEx(acp.kvdatas)
+
+	kvs := genKVs(10)
+	for _, kv := range kvs {
+		if kv.delete {
+			batch.Delete(kv.key)
+		} else {
+			batch.Put(kv.key, kv.value)
+		}
+	}
+	assert.Equal(t, len(batch.writes), 10)
+	batch.Write()
+	batch.Reset()
+	assert.Equal(t, batch.ValueSize(), 0)
+
+	time.Sleep(time.Second)
+
+	for _, kv := range kvs {
+		v, err := diskdb.Get(kv.key)
+		if kv.delete {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, kv.value, v)
+		}
+	}
+}
+
+func TestACIterator(t *testing.T) {
+	triedb := NewDatabaseWithConfig(memorydb.New(), &Config{
+		Preimages: true,
+		EnableAC:  true,
+	})
+	ctr, _ := New(common.Hash{}, triedb)
+	for _, val := range testdata1 {
+		ctr.Update([]byte(val.k), []byte(val.v))
+	}
+	root, _ := ctr.Commit(nil)
+
+	triedb.Commit(root, true, nil)
+
+	trie, err := New(root, triedb)
+	assert.NoError(t, err)
+
+	// should stop the ACCommit() for test
+	found := make(map[string]string)
+	it := NewIterator(trie.NodeIterator(nil))
+	for it.Next() {
+		found[string(it.Key)] = string(it.Value)
+	}
+
+	for _, kv := range testdata1 {
+		if found[kv.k] != kv.v {
+			t.Errorf("iterator value mismatch for %s: got %q want %q", kv.k, found[kv.v], kv.v)
+		}
+	}
+}
+
+func TestCacheList(t *testing.T) {
+	preImages := genTestImages(1000)
+	dirties := genTestDirties(1000)
+
+	clist := NewCacheList()
+
+	clist.SetPreimages(preImages)
+	for _, n := range dirties {
+		clist.SetDirty(common.BytesToHash(n.key), n.node)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for _, ts := range dirties {
+			ca, re := clist.GetDirty(common.BytesToHash(ts.key))
+			assert.True(t, re)
+			assert.Equal(t, ca, ts.node)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for k, v := range preImages {
+			ca, re := clist.GetPreimage(k)
+			assert.True(t, re)
+			assert.Equal(t, ca, v)
+		}
+	}()
+	wg.Wait()
+
+	// clear
+	var kvs []*keyvalue
+	for k := range preImages {
+		key := make([]byte, len(k))
+		copy(key, k[:])
+		kvs = append(kvs, &keyvalue{key: key})
+	}
+	for _, k := range dirties {
+		kvs = append(kvs, &keyvalue{key: k.key})
+	}
+
+	clist.Clear(kvs)
+	// check
+	for _, ca := range clist.caches {
+		assert.Equal(t, len(ca.preimages), 0)
+		assert.Equal(t, len(ca.dirties), 0)
+	}
+}
